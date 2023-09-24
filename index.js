0a54726887c3df2e8246520065e152ff03251d31
Svitlana Andrushenko <wwwli4e@gmail.com>
2023-09-24 22:56:27 +0300

    feat: duration time, current time, progress bar added

--- index.js
+++ index.js
@@ -9,21 +9,26 @@ const title = document.querySelector('.song');
 const singerName = document.querySelector('singer');
 const cover = document.querySelector('.img1');
 const playPause = document.querySelector('.play-pause');
-progressBar = document.querySelector('.progress')
-
+const progress = document.querySelector('.progress')
+const bigCover = document.querySelector('.cover-img')
+const durationTime = document.querySelector('.duration-time');
+const current = document.querySelector('.current-time')
+const progressContainer = document.querySelector('progress-container')
 //Названия песен
 const songs = ['Don\'t Hurt Yourself', 'Don\'t Start Now'];
 const singers = ['Beyonce', 'Dua Lipa']
 
 //Песня по умолчанию
 let songIndex = 0;
-let singerIndex = 1;
-
+let singerIndex = 0;
+ 
 //
 function loadSong(song){
     title.innerHTML = song
+    
     audio.src = `audio/${song}.mp3`
     cover.src = `img/cover${songIndex + 1}.png`
+    bigCover.src = `img/bigcover${songIndex + 1}.png`
 }
 loadSong(songs[songIndex]);
 
@@ -75,8 +80,23 @@ prevBtn.addEventListener('click', prevSong)
 
 // Progress bar
 function updateProgress(event){
+    
     const {duration, currentTime} = event.srcElement
     const progressPersent = (currentTime / duration) * 100
     progress.style.width = `${progressPersent}%`
+    progress.max = durationTime.innerHTML
+    durationTime.innerHTML = parseFloat((duration / 60 -0.01 ).toFixed(2));
+    current.innerHTML = parseFloat((currentTime / 60 - 0.001).toFixed(2));
 }
+audio.addEventListener('timeupdate', updateProgress)
+
+// Set progress
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+}
+
+progressContainer.addEventListener('click', setProgress)
 })
